02b45869504b2b1c12ecd765e508b27307b377a6
Blake Blackwell <blakewell@gmail.com>
2017-04-15 15:54:38 -0500

    changes

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -16,10 +16,10 @@ object Main {
     * Exercise 1
     */
   def pascal(c: Int, r: Int): Int = {
-    if (c <= 0 || c >= r) 1
+    if (c <= 0 || c >= r) 1 //pascal trial always has ones on the sides
     else {
-      val prevR = r - 1
-      pascal(c, prevR) + pascal(c - 1, prevR)
+      val prevR = r - 1 //calculate once
+      pascal(c, prevR) + pascal(c - 1, prevR) //pascal's triangle depends on the row above
     }
 
   }
@@ -29,14 +29,14 @@ object Main {
     */
   def balance(chars: List[Char]): Boolean =  {
 
-    def iterate(chars: List[Char], acc: Int): Int = {
+    def balanceAcc(chars: List[Char], acc: Int): Int = {
       if (chars.isEmpty || acc < 0) acc
-      else if (chars.head == '(') iterate(chars.tail, acc + 1)
-      else if (chars.head == ')') iterate(chars.tail, acc - 1)
-      else iterate(chars.tail, acc)
+      else if (chars.head == '(') balanceAcc(chars.tail, acc + 1)
+      else if (chars.head == ')') balanceAcc(chars.tail, acc - 1)
+      else balanceAcc(chars.tail, acc)
     }
 
-    iterate(chars, 0) == 0
+    balanceAcc(chars, 0) == 0 //move through string character by character starting at beginning
   }
 
 
@@ -44,10 +44,11 @@ object Main {
     * Exercise 3
     */
   def countChange(money: Int, coins: List[Int]): Int =  {
-    if (money == 0) 1
-    else if (money < 0 || coins.isEmpty) 0
-    else if (money <= 0 && !coins.isEmpty) 0
-    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+
+    if (money == 0) 1 //return 1 when there is no money (only one way to calculate)
+    else if (money < 0 || coins.isEmpty) 0 //no money or no coins results in 0 possibilities
+    else if (money <= 0 && !coins.isEmpty) 0 //no money and coins results in 0 possibilities
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins) //use recursion to iterate down
   }
 
 }
